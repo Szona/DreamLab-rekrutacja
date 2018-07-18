7577d1665a76794f94c494b83f9a694fce1f6c2c
Szona <cmielalicja@gmail.com>
2018-07-18 15:40:52 +0200

    basic styles

--- animation/app/app.js
+++ animation/app/app.js
@@ -1,1 +1,18 @@
-console.log("asdsada");
+$(document).ready(function () {
+
+    const item = $(".circle-box__circle--active");
+
+    item.click(function(){
+        $(this).removeClass("circle-box__circle--active").next().addClass("circle-box__circle--active");
+        if (item.hasClass("circle-box__circle--active") == false) {
+            item.first().addClass("circle-box__circle--active");
+        }
+        if (item.last().hasClass("circle-box__circle--active")) {
+            item.last().addClass(("circle-box__circle--done"));
+        }
+    });
+
+    
+
+
+});
