6e219442f9341549261d94b6c5fc12628ffa27a8
Szona <cmielalicja@gmail.com>
2018-07-20 12:14:25 +0200

    animation: change position

--- animation/app/app.js
+++ animation/app/app.js
@@ -1,16 +1,31 @@
 $(document).ready(function () {
 
-    const item = $(".circle-box__circle--active");
+    const activeItem = $(".one");
+    const itemTwo = $(".two");
+    const itemThree = $(".three");
 
-    item.click(function(){
-        $(this).removeClass("circle-box__circle--active").next().addClass("circle-box__circle--active");
-        if (item.hasClass("circle-box__circle--active") == false) {
-            item.first().addClass("circle-box__circle--active");
-        }
-        if (item.last().hasClass("circle-box__circle--active")) {
-            item.last().addClass(("circle-box__circle--done"));
+    let clickCount = 0;
+    
+    activeItem.click(function(){
+        clickCount += 1;
+        if (clickCount === 1) {
+            activeItem.addClass("step1");
+            itemTwo.addClass("step1");
+            itemThree.addClass("step1");
+        } else if (clickCount == 2) {
+            activeItem.addClass("step2");
+            itemThree.addClass("step2");
+            activeItem.addClass("circle-box__circle--done")
+        } else if (clickCount === 3) {
+            clickCount = 0;
+            activeItem.removeClass("circle-box__circle--done");
+            activeItem.removeClass("step1 step2");
+            itemTwo.removeClass("step1");
+            itemThree.removeClass("step1 step2");
         }
     });
+
+    
 
     
 
